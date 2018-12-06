b9102fb371136a219a7aaec57f23711327fb967e
sharifahmad <sharifahmad2061@gmail.com>
2018-12-06 15:15:25 +0500

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+namespace Client
+{
+    class Program
+    {
+        private static IPAddress ActiveIp()
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation unicastIPAddressInformation in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (unicastIPAddressInformation.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            return unicastIPAddressInformation.Address;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any,2222);
+            UdpClient udpClient = new UdpClient(iPEndPoint);
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket,SocketOptionName.MulticastInterface,1);
+            IPAddress iPAddress = IPAddress.Parse("232.0.0.2");
+            udpClient.JoinMulticastGroup(iPAddress);
+            
+            Console.WriteLine("Listening this will never quit so you will need to ctrl-c it");
+
+            Byte[] msg = udpClient.Receive(ref iPEndPoint);
+            Console.WriteLine(Encoding.Unicode.GetString(msg));
+
+            while (true)
+            {
+                Byte[] data = udpClient.Receive(ref iPEndPoint);
+                string strData = Encoding.Unicode.GetString(data);
+                Console.WriteLine(strData);
+            }
+
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPAddress iPAddress = IPAddress.Parse("232.0.0.2");
+            udpClient.JoinMulticastGroup(iPAddress);
+
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 2222);
+
+            Console.WriteLine("Press ENTER to start sending messages");
+            Console.ReadLine();
+
+            Byte[] buffer = null;
+
+            buffer = Encoding.Unicode.GetBytes("hello there");
+            udpClient.Send(buffer, buffer.Length, iPEndPoint);
+
+            for (int i = 0; i <= 8000; i++)
+            {
+                buffer = Encoding.Unicode.GetBytes(i.ToString());
+                udpClient.Send(buffer, buffer.Length, iPEndPoint);
+                Console.WriteLine("Sent " + i);
+            }
+
+            Console.WriteLine("All Done! Press ENTER to quit.");
+            Console.ReadLine();
+        }
+    }
+}
