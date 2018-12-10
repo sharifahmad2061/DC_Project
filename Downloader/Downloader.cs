00dee1d07a12373305a1fcc96cc25d5ee08db9b3
mranon0007 <mranon0007@gmail.com>
2018-12-10 11:37:29 +0500

    Downloader Class Created

---
+++ Downloader/Downloader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Downloader
+{
+    public class Downloader
+    {
+        string DownloadFile(int StartByte, int EndByte)
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            return "";
+        }
+
+        string JoinParts(String[] FilePartsPathsInSortedArray)
+        {
+            try
+            {
+            
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            return "";
+        }
+    }
+}
